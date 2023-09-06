fa8dacb3712959de43bc960f3de4e32ee5a97122
Goshen Jimenez <gjimenez@csmbataan.ph.education>
2023-09-06 14:32:56 +0800

    Add my Manage Page

---
+++ CSMWebsite2023/CSMWebsite2023.Web/Pages/Manage/Users/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSMWebsite2023.Web.Manage.Users
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
