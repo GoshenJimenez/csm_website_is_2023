de4b3cf1754d28aff47000523122757486f6f614
John Lawrence Lapid <jlapid@csmbataan.ph.education>
2023-09-13 16:49:09 +0800

    MyFirstCommit

---
+++ CSMWebsite2023/CSMWebsite2023.Web/Pages/Manage/CsmGroup/Index.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSMWebsite2023.Web.Pages.Manage.CsmGroup
+{
+    public class IndexModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
