c4675b0ac448037d871d2c857472ac2f251ef681
dary john silvestre <djsilvestre@csmbataan.ph.education>
2023-10-02 16:48:54 +0800

    Regster Models in DbContext

---
+++ CSMWebsite2023/CSMWebsite2023.Web/Pages/Manage/SchoolAds/AdShares.cs
@@ -0,0 +1,8 @@
+﻿namespace CSMWebsite2023.Web.Pages.Manage.SchoolAds
+{
+    public class AdShares
+    {
+        public Guid? AdID { get; set; }
+        public Guid? UserId { get; set; }
+    }
+}
