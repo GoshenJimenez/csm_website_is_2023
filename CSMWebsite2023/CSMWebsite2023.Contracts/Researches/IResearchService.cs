b4401b20003c2b93314ec264a771bb0bac219da7
Neil Armstrong <nmacabulos@csmbataan.ph.education>
2023-10-09 14:39:50 +0800

    Research Contract in contracts Project

---
+++ CSMWebsite2023/CSMWebsite2023.Contracts/Researches/IResearchService.cs
@@ -0,0 +1,15 @@
+﻿using CSMWebsite2023.Contracts.Chats;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMWebsite2023.Contracts.Researches
+{
+    public interface IResearchService : IService
+    {
+        List<ResearchDto>? GetResearches();
+        ResearchDto? GetResearchById(Guid? id);
+    }
+}
---
+++ CSMWebsite2023/CSMWebsite2023.Contracts/Researches/ResearchMap.cs
@@ -0,0 +1,20 @@
+﻿using AutoMapper;
+using CSMWebsite2023.Contracts.Chats;
+using CSMWebsite2023.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSMWebsite2023.Contracts.Researches
+{
+    public class Mapper : Profile
+    {
+        public Mapper()
+        {
+            CreateMap<Research, ResearchDto>();
+            CreateMap<ResearchDto, Research>();
+        }
+    }
+}
