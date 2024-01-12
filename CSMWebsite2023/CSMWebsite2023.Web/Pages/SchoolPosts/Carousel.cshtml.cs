282740f9cafcae12a6cd3ad9bc6ca31f28098625
Goshen Jimenez <gjimenez@csmbataan.ph.education>
2024-01-12 14:41:56 +0800

    Creating new pages

---
+++ CSMWebsite2023/CSMWebsite2023.Web/Pages/SchoolPosts/Carousel.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace CSMWebsite2023.Web.Pages.SchoolPosts
+{
+    public class Carousel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
